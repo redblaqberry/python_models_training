f16b727aac3b574ca6ca6562b0b9f6ffb72ac10d
redblaqberry <133053412+redblaqberry@users.noreply.github.com>
2024-05-27 15:31:03 +0200

    Update vgg16cifar10label.py

--- vgg16cifar10label.py
+++ vgg16cifar10label.py
@@ -7,6 +7,13 @@ Original file is located at
     https://colab.research.google.com/drive/1ZIw4znzpijH5IkBXcAfuTbTDoyivU6FH
 """
 
+
+import os
+NUM_WORKERS = 20
+os.environ['MKL_NUM_THREADS'] = str(NUM_WORKERS)
+os.environ['NUMEXPR_NUM_THREADS'] = str(NUM_WORKERS)
+os.environ['OMP_NUM_THREADS'] = str(NUM_WORKERS)
+os.environ["CUDA_VISIBLE_DEVICES"] = str(0)
 import torch
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
@@ -20,14 +27,7 @@ from tqdm import tqdm
 from torchsummary import summary
 from pathlib import Path
 
-import os
-NUM_WORKERS = 20
-os.environ['MKL_NUM_THREADS'] = str(NUM_WORKERS)
-os.environ['NUMEXPR_NUM_THREADS'] = str(NUM_WORKERS)
-os.environ['OMP_NUM_THREADS'] = str(NUM_WORKERS)
-os.environ["CUDA_VISIBLE_DEVICES"] = str(0)
-
-Path('/sub_folder').mkdir(parents=True, exist_ok=True)
+Path('./sub_folder').mkdir(parents=True, exist_ok=True)
 
 # Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -167,4 +167,4 @@ for i, percentage_to_change in enumerate(range(0, 101, 10)):
         'learning_rate': optimizer.param_groups[0]["lr"]
     }
 
-    torch.save(training_stats, f"/sub_folder/cifar10_label_vgg16_{i}.pth")
+    torch.save(training_stats, f"./sub_folder/cifar10_label_vgg16_{i}.pth")
